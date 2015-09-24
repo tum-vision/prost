6f218230d8f887056bd1c279f7108f0d56d28913
Thomas Möllenhoff <moellenh@in.tum.de>
2015-09-24 12:11:01 +0200

    energy computation in examples

--- examples/ex_rof.m
+++ examples/ex_rof.m
@@ -2,13 +2,13 @@
 % min_u (1/2) (u-f)^2 + \lambda * ||\nabla u||_{2,1}
 
 %% read data, build linear operator
-im = imread('data/surprised-cat.jpg');
-im = imresize(im, 0.5);
+im = imread('data/24004.jpg');
+im = imresize(im, 1);
 [ny, nx] = size(im);
 
 f = double(im(:)) / 255.;
 K = grad_forw_2d(nx, ny, 1);
-lmb = 0.5; 
+lmb = 0.25; 
 
 %% setup prox operators
 %prox_g = { prox_1d(0, nx * ny, 'square', 1, f, 1, 0, 0) };
@@ -25,18 +25,26 @@ prox_g = { prox_moreau(prox_moreau(prox_1d(0, nx * ny, 'square', 1, f, 1, 0, 0))
 prox_hstar = { prox_moreau(prox_norm2(0, nx * ny, 2, false, 'abs', ...
                                       lmb, 0, 1, 0, 0)) }; 
 
+global plot_primal;
+global plot_dual;
+global plot_iters;
+
+plot_primal=[];
+plot_dual=[];
+plot_iters=[];
+
 %% set options and run algorithm
 opts = pdsolver_opts();
-opts.adapt = 'none';
+opts.adapt = 'balance';
 opts.verbose = false;
 opts.bt_enabled = false;
-opts.max_iters = 1000;
-opts.cb_iters = 10;
+opts.max_iters = 10000;
+opts.cb_iters = 100;
 opts.precond = 'alpha';
 opts.precond_alpha = 1.;
 opts.tol_primal = 0.01;
 opts.tol_dual = 0.01;
-opts.callback = @(it, x, y) ex_rof_callback(K, f, lmb, it, x, y);
+opts.callback = @(it, x, y) ex_rof_callback(K, f, lmb, it, x, y, t);
 [x, y] = pdsolver(K, prox_g, prox_hstar, opts);
 
 %% show result
