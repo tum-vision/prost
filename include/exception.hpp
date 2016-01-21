ffe619bf5dc855c2c4da4c3e5531f140d37ab23e
Thomas Möllenhoff <moellenh@in.tum.de>
2016-01-21 12:09:17 +0100

    major refactoring

--- include/exception.hpp
+++ include/exception.hpp
@@ -1,5 +1,21 @@
 #ifndef EXCEPTION_HPP_
 #define EXCEPTION_HPP_
 
-#endif
+#include <stdexcept>
+#include <string>
 
+class Exception : public std::exception {
+public:
+  explicit Exception(const char *message) : msg_(message) {}
+  explicit Exception(const std::string& message) : msg_(message) {}
+  virtual ~Exception() throw () {}
+
+  virtual const char* what() const throw() {
+    return msg_.c_str();
+  }
+
+protected:
+  std::string msg_;
+};
+
+#endif
