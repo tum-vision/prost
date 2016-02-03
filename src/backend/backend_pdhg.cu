8503898e67a4e363f674bca93f96c7ac4f8a3f25
Thomas Möllenhoff <moellenh@in.tum.de>
2016-02-03 21:54:45 +0100

    minor fixes

--- src/backend/backend_pdhg.cu
+++ src/backend/backend_pdhg.cu
@@ -11,6 +11,7 @@
 #include "exception.hpp"
 #include "problem.hpp"
 
+/// \brief 
 template<typename T>
 struct primal_proxarg_functor
 {
@@ -30,14 +31,14 @@ template<typename T>
 struct dual_proxarg_functor
 {
   __host__ __device__ dual_proxarg_functor(T sigma, T theta) 
-    : sigma_(sigma), theta_(theta) { }
+      : sigma_(sigma), theta_(theta) { }
 
   template <typename Tuple>
   __host__ __device__
   void operator()(Tuple t)
   {
     thrust::get<4>(t) = thrust::get<0>(t) + sigma_ * thrust::get<1>(t) * 
-      ((1 + theta_) * thrust::get<2>(t) - theta_ * thrust::get<3>(t));
+                        ((1 + theta_) * thrust::get<2>(t) - theta_ * thrust::get<3>(t));
   }
 
   T sigma_;
@@ -51,14 +52,14 @@ struct dual_residual_transform : public thrust::unary_function<thrust::tuple<T,T
   typedef typename thrust::tuple<T,T> OutputTuple;
 
   __host__ __device__ dual_residual_transform(T tau)
-    : tau_(tau) { }
+  : tau_(tau) { }
 
   __host__ __device__
   OutputTuple operator()(const InputTuple& t)
   {
     const T tau_diag = thrust::get<2>(t);
     const T w_hat = (thrust::get<0>(t) - thrust::get<1>(t)) / (tau_ * sqrt(tau_diag)) - 
-      sqrt(tau_diag) * thrust::get<3>(t);
+                    sqrt(tau_diag) * thrust::get<3>(t);
     const T diff = w_hat + sqrt(tau_diag) * thrust::get<4>(t); // w_hat^{k+1} + T K^T y^{k+1}
 
     return OutputTuple(diff * diff, w_hat * w_hat);
@@ -74,14 +75,14 @@ struct primal_residual_transform : public thrust::unary_function<thrust::tuple<T
   typedef typename thrust::tuple<T,T> OutputTuple;
 
   __host__ __device__ primal_residual_transform(T sigma, T theta)
-    : sigma_(sigma), theta_(theta) { }
+      : sigma_(sigma), theta_(theta) { }
 
   __host__ __device__
   OutputTuple operator()(const InputTuple& t)
   {
     const T sigma_diag = thrust::get<2>(t);
     const T z_hat = (thrust::get<0>(t) - thrust::get<1>(t)) / (sigma_ * sqrt(sigma_diag)) +
-      sqrt(sigma_diag) * ((1 + theta_) * thrust::get<4>(t) - theta_ * thrust::get<3>(t));
+                    sqrt(sigma_diag) * ((1 + theta_) * thrust::get<4>(t) - theta_ * thrust::get<3>(t));
       
     const T diff = z_hat - sqrt(sigma_diag) * thrust::get<4>(t);
 
@@ -94,8 +95,8 @@ struct primal_residual_transform : public thrust::unary_function<thrust::tuple<T
 
 template<typename T>
 struct residual_tuple_sum : public thrust::binary_function< thrust::tuple<T, T>, 
-                                                            thrust::tuple<T, T>,
-                                                            thrust::tuple<T, T> >
+                                                              thrust::tuple<T, T>,
+  thrust::tuple<T, T> >
 {
   typedef typename thrust::tuple<T, T> Tuple;
 
@@ -103,14 +104,14 @@ struct residual_tuple_sum : public thrust::binary_function< thrust::tuple<T, T>,
   Tuple operator()(const Tuple& t0, const Tuple& t1) const
   {
     return Tuple(
-      thrust::get<0>(t0) + thrust::get<0>(t0), 
-      thrust::get<1>(t1) + thrust::get<1>(t1));
+        thrust::get<0>(t0) + thrust::get<0>(t0), 
+        thrust::get<1>(t1) + thrust::get<1>(t1));
   }
 };
 
 template<typename T>
 BackendPDHG<T>::BackendPDHG(const typename BackendPDHG<T>::Options& opts)
-  : opts_(opts)
+    : opts_(opts)
 {
 }
 
@@ -197,21 +198,22 @@ BackendPDHG<T>::PerformIteration()
   if(!opts_.solve_dual_problem)
   {
     // compute primal prox arg into temp_
+    // thrust::get<3>(t) = thrust::get<0>(t) - tau_ * thrust::get<1>(t) * thrust::get<2>(t);
     thrust::for_each(
 
-      thrust::make_zip_iterator(thrust::make_tuple(
-          x_.begin(), 
-          this->problem_->scaling_right().begin(), 
-          kty_.begin(), 
-          temp_.begin())),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            x_.begin(), 
+            this->problem_->scaling_right().begin(), 
+            kty_.begin(), 
+            temp_.begin())),
 
-      thrust::make_zip_iterator(thrust::make_tuple(
-          x_.end(), 
-          this->problem_->scaling_right().end(), 
-          kty_.end(), 
-          temp_.end())),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            x_.end(), 
+            this->problem_->scaling_right().end(), 
+            kty_.end(), 
+            temp_.end())),
 
-      primal_proxarg_functor<T>(tau_));
+        primal_proxarg_functor<T>(tau_));
 
     // remember previous primal iterate
     x_.swap(x_prev_);
@@ -227,24 +229,24 @@ BackendPDHG<T>::PerformIteration()
     this->problem_->linop()->Eval(kx_, x_);
 
     // compute dual prox arg
+    // thrust::get<4>(t) = thrust::get<0>(t) + sigma_ * thrust::get<1>(t) * 
+    //                     ((1 + theta_) * thrust::get<2>(t) - theta_ * thrust::get<3>(t));
     thrust::for_each(
-      thrust::make_zip_iterator(thrust::make_tuple(
-          y_.begin(),
-          this->problem_->scaling_right().begin(),
-          kx_.begin(),
-          kx_prev_.begin(),
-          temp_.begin()
-          )),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            y_.begin(),
+            this->problem_->scaling_left().begin(),
+            kx_.begin(),
+            kx_prev_.begin(),
+            temp_.begin())),
 
-      thrust::make_zip_iterator(thrust::make_tuple(
-          y_.end(),
-          this->problem_->scaling_right().end(),
-          kx_.end(),
-          kx_prev_.end(),
-          temp_.end()
-          )),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            y_.end(),
+            this->problem_->scaling_left().end(),
+            kx_.end(),
+            kx_prev_.end(),
+            temp_.end())),
 
-      dual_proxarg_functor<T>(sigma_, theta_));
+        dual_proxarg_functor<T>(sigma_, theta_));
 
     // apply prox_fstar
     for(auto& p : prox_fstar_)
@@ -269,44 +271,44 @@ BackendPDHG<T>::PerformIteration()
     // compute primal residual |Kx - z|^2 and norm |z|^2
     thrust::tuple<T, T> primal = thrust::transform_reduce(
 
-      thrust::make_zip_iterator(thrust::make_tuple(
-          y_prev_.begin(),
-          y_.begin(),
-          this->problem_->scaling_left().begin(),
-          kx_prev_.begin(),
-          kx_.begin())),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            y_prev_.begin(),
+            y_.begin(),
+            this->problem_->scaling_left().begin(),
+            kx_prev_.begin(),
+            kx_.begin())),
 
-      thrust::make_zip_iterator(thrust::make_tuple(
-          y_prev_.end(),
-          y_.end(),
-          this->problem_->scaling_left().end(),
-          kx_prev_.end(),
-          kx_.end())),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            y_prev_.end(),
+            y_.end(),
+            this->problem_->scaling_left().end(),
+            kx_prev_.end(),
+            kx_.end())),
 
-      primal_residual_transform<T>(sigma_, theta_),
-      thrust::tuple<T, T>(0, 0),
-      residual_tuple_sum<T>());
+        primal_residual_transform<T>(sigma_, theta_),
+        thrust::tuple<T, T>(0, 0),
+        residual_tuple_sum<T>());
 
     // compute dual residual |K^T y + w|^2 and norm |w|^2
     thrust::tuple<T, T> dual = thrust::transform_reduce(
 
-      thrust::make_zip_iterator(thrust::make_tuple(
-          x_prev_.begin(),
-          x_.begin(),
-          this->problem_->scaling_right().begin(),
-          kty_prev_.begin(),
-          kty_.begin())),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            x_prev_.begin(),
+            x_.begin(),
+            this->problem_->scaling_right().begin(),
+            kty_prev_.begin(),
+            kty_.begin())),
 
-      thrust::make_zip_iterator(thrust::make_tuple(
-          x_prev_.end(),
-          x_.end(),
-          this->problem_->scaling_right().end(),
-          kty_prev_.end(),
-          kty_.end())),
+        thrust::make_zip_iterator(thrust::make_tuple(
+            x_prev_.end(),
+            x_.end(),
+            this->problem_->scaling_right().end(),
+            kty_prev_.end(),
+            kty_.end())),
 
-      dual_residual_transform<T>(tau_),
-      thrust::tuple<T, T>(0, 0),
-      residual_tuple_sum<T>());
+        dual_residual_transform<T>(tau_),
+        thrust::tuple<T, T>(0, 0),
+        residual_tuple_sum<T>());
     
     this->primal_residual_ = std::sqrt(thrust::get<0>(primal));
     this->primal_var_norm_ = std::sqrt(thrust::get<1>(primal));
@@ -318,53 +320,53 @@ BackendPDHG<T>::PerformIteration()
 
     switch(opts_.stepsize_variant)
     {
-    case BackendPDHG<T>::StepsizeVariant::kPDHGStepsResidualGoldstein:
-      if( this->dual_residual_ > (this->primal_residual_ * opts_.arg_delta) )
-      {
-        tau_ = tau_ / (1 - arg_alpha_);
-        sigma_ = sigma_ * (1 - arg_alpha_);
-        arg_alpha_ = arg_alpha_ * opts_.arg_nu;
-      }
+      case BackendPDHG<T>::StepsizeVariant::kPDHGStepsResidualGoldstein:
+        if( this->dual_residual_ > (this->primal_residual_ * opts_.arg_delta) )
+        {
+          tau_ = tau_ / (1 - arg_alpha_);
+          sigma_ = sigma_ * (1 - arg_alpha_);
+          arg_alpha_ = arg_alpha_ * opts_.arg_nu;
+        }
 
-      if( this->dual_residual_ < (this->primal_residual_ / opts_.arg_delta) )
-      {
-        tau_ = tau_ * (1 - arg_alpha_);
-        sigma_ = sigma_ / (1 - arg_alpha_);
-        arg_alpha_ = arg_alpha_ * opts_.arg_nu;
-      }
+        if( this->dual_residual_ < (this->primal_residual_ / opts_.arg_delta) )
+        {
+          tau_ = tau_ * (1 - arg_alpha_);
+          sigma_ = sigma_ / (1 - arg_alpha_);
+          arg_alpha_ = arg_alpha_ * opts_.arg_nu;
+        }
       
-      break;
+        break;
 
-    case BackendPDHG<T>::StepsizeVariant::kPDHGStepsResidualBoyd:
-      // TODO: test this
-      if( (this->dual_residual_ < eps_dual) && (opts_.arb_tau * iteration_ > arb_l_) )
-      {
-        tau_ *= opts_.arb_delta;
-        sigma_ /= opts_.arb_delta;
-        arb_u_ = iteration_;
-      }
-      else if( (this->primal_residual_ < eps_primal) && (opts_.arb_tau * iteration_ > arb_u_) )
-      {
-        tau_ /= opts_.arb_delta;
-        sigma_ *= opts_.arb_delta;
-        arb_l_ = iteration_;
-      }
+      case BackendPDHG<T>::StepsizeVariant::kPDHGStepsResidualBoyd:
+        // TODO: test this
+        if( (this->dual_residual_ < eps_dual) && (opts_.arb_tau * iteration_ > arb_l_) )
+        {
+          tau_ *= opts_.arb_delta;
+          sigma_ /= opts_.arb_delta;
+          arb_u_ = iteration_;
+        }
+        else if( (this->primal_residual_ < eps_primal) && (opts_.arb_tau * iteration_ > arb_u_) )
+        {
+          tau_ /= opts_.arb_delta;
+          sigma_ *= opts_.arb_delta;
+          arb_l_ = iteration_;
+        }
 
-      break;
+        break;
 
-    case BackendPDHG<T>::StepsizeVariant::kPDHGStepsCallback:
-    {
-      // TODO: test this
-      double new_tau = tau_;
-      double new_sigma = sigma_;
+      case BackendPDHG<T>::StepsizeVariant::kPDHGStepsCallback:
+        {
+          // TODO: test this
+          double new_tau = tau_;
+          double new_sigma = sigma_;
 
-      stepsize_cb_(iteration_, this->primal_residual_, this->dual_residual_, new_tau, new_sigma);
-      tau_ = new_tau;
-      sigma_ = new_sigma;
-    } break;
+          stepsize_cb_(iteration_, this->primal_residual_, this->dual_residual_, new_tau, new_sigma);
+          tau_ = new_tau;
+          sigma_ = new_sigma;
+        } break;
 
-    default:
-      break;
+      default:
+        break;
     }
   }
 
