405364aaeb581df263e680d8e071c75b017f3bbc
Thomas Möllenhoff <moellenh@in.tum.de>
2016-02-09 18:12:41 +0100

    Fixed bug in normest. Fixed bug in Preconditioners. Color ROF example.

--- matlab/examples/example_rof.m
+++ matlab/examples/example_rof.m
@@ -1,30 +1,32 @@
-im = imread('../../images/24004.jpg');
+im = imread('../../images/dog.png');
 im = imresize(im, 1);
 [ny, nx, nc] = size(im);
-N = nx * ny * nc;
 f = double(im(:)) / 255.;
-grad = spmat_gradient2d(nx, ny, nc);
+grad = spmat_gradient2d(nx,ny,nc);
 lmb = 1;
 
 % create problem description
 prob = prost.pdsolver_problem();
 prob.linop = { prost.block_sparse(0, 0, grad) };
-prob.prox_g = { prost.prox_1d(0, N, 'square', 1, f, 1, 0, 0) };
-prob.prox_fstar = { prost.prox_norm2(0, N, 2, false, 'ind_leq0', ...
+prob.prox_g = { prost.prox_1d(0, nx * ny * nc, 'square', 1, f, 1, 0, 0) };
+prob.prox_fstar = { prost.prox_norm2(0, nx * ny, 2 * nc, false, 'ind_leq0', ...
                                1 / lmb, 1, 1, 0, 0) };
-prob.scaling = 'identity';
+prob.scaling = 'alpha';
 
 % create backend
 backend = prost.pdsolver_backend_pdhg(...
     'residual_iter', 10, ...
-    'stepsize', 'alg1' );
+    'stepsize', 'boyd', ...
+    'scale_steps_operator', true);
 
 % specify solver options
 opts = prost.pdsolver_options();
 opts.max_iters = 10000;
 
 % solve problem
+tic;
 solution = prost.prost_(prob, backend, opts);
+toc;
 
 figure;
 imshow(reshape(solution.x, ny, nx, nc));
