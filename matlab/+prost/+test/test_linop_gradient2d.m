f39c3dc95a3aa9c87c936795b70f680720420535
Thomas Möllenhoff <moellenh@in.tum.de>
2016-02-12 11:36:56 +0100

    added gradient operator & tests.

---
+++ matlab/+prost/+test/test_linop_gradient2d.m
@@ -0,0 +1,50 @@
+function [passed] = test_linop_gradient2d()
+    
+    nx = 307;
+    ny = 229;
+    L = 8;
+
+    linop_2d = { prost.block.gradient2d(0, 0, nx, ny, L) };
+    K_2d = spmat_gradient2d(nx, ny, L);
+
+    inp = rand(nx*ny*L, 1);
+    inp2 = rand(nx*ny*L*2, 1);
+
+    [x,~,~] = prost.eval_linop(linop_2d, inp, false);
+    [y,rowsum,colsum] = prost.eval_linop(linop_2d, inp2, true);
+
+    x_ml = K_2d * inp;
+    y_ml = K_2d' * inp2;
+
+    rowsum_ml = sum(abs(K_2d), 2);
+    colsum_ml = sum(abs(K_2d), 1)';
+
+    if(norm(x - x_ml) > 1e-3)
+        fprintf('failed! Reason: gradient2d_norm_diff_forward: %f\n', ...
+                norm(x - x_ml));
+        passed = false;
+        return;
+    end
+
+    if(norm(y - y_ml) > 1e-3)
+        fprintf('failed! Reason: gradient2d_norm_diff_adjoint: %f\n', ...
+                norm(y - y_ml));
+        passed = false;
+        return;
+    end
+
+    if(full(sum(rowsum<rowsum_ml)) > 1e-3)
+        fprintf('failed! Reason: gradient2d_diff_rowsum: %f\n', ...
+                full(sum(rowsum<rowsum_ml)));
+        passed = false;
+    end
+
+    if(full(sum(colsum<colsum_ml)) > 1e-3)
+        fprintf('failed! Reason: gradient2d_diff_rowsum: %f\n', ...
+                full(sum(colsum<colsum_ml)));
+        passed = false;
+    end
+
+    passed = true;
+    
+end
---
+++ matlab/+prost/+test/test_linop_gradient3d.m
@@ -0,0 +1,50 @@
+function [passed] = test_linop_gradient3d()
+    
+    nx = 491;
+    ny = 843;
+    L = 17;
+
+    linop_3d = { prost.block.gradient3d(0, 0, nx, ny, L) };
+    K_3d = spmat_gradient3d(nx, ny, L);
+
+    inp = rand(nx*ny*L, 1);
+    inp2 = rand(nx*ny*L*3, 1);
+
+    [x,~,~] = prost.eval_linop(linop_3d, inp, false);
+    [y,rowsum,colsum] = prost.eval_linop(linop_3d, inp2, true);
+
+    x_ml = K_3d * inp;
+    y_ml = K_3d' * inp2;
+
+    rowsum_ml = sum(abs(K_3d), 2);
+    colsum_ml = sum(abs(K_3d), 1)';
+
+    if(norm(x - x_ml) > 1e-3)
+        fprintf('failed! Reason: gradient3d_norm_diff_forward: %f\n', ...
+                norm(x - x_ml));
+        passed = false;
+        return;
+    end
+
+    if(norm(y - y_ml) > 1e-3)
+        fprintf('failed! Reason: gradient3d_norm_diff_adjoint: %f\n', ...
+                norm(y - y_ml));
+        passed = false;
+        return;
+    end
+
+    if(full(sum(rowsum<rowsum_ml)) > 1e-3)
+        fprintf('failed! Reason: gradient3d_diff_rowsum: %f\n', ...
+                full(sum(rowsum<rowsum_ml)));
+        passed = false;
+    end
+
+    if(full(sum(colsum<colsum_ml)) > 1e-3)
+        fprintf('failed! Reason: gradient3d_diff_rowsum: %f\n', ...
+                full(sum(colsum<colsum_ml)));
+        passed = false;
+    end
+
+    passed = true;
+    
+end
