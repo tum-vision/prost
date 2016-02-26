997f8231668d036f91fb805a250a89e60ef5d679
Thomas Möllenhoff <moellenh@in.tum.de>
2016-02-26 17:53:44 +0100

    Polyhedral prox in 2D works with Active-Set.

--- matlab/+prost/+prox/sum_ind_epi_polyhedral.m
+++ matlab/+prost/+prox/sum_ind_epi_polyhedral.m
@@ -1,9 +1,9 @@
-function [prox] = sum_ind_epi_polyhedral(idx, count, dim, a, b, count_vec, index_vec)
+function [prox] = sum_ind_epi_polyhedral(idx, count, dim, interleaved, a, b, count_vec, index_vec)
 % computes projection onto epigraph of function given as maximum
 % over linear functions specified by a, b, count_vec, index_vec.
     
     coeffs = { a, b, count_vec, index_vec };
     prox = { 'ind_epi_polyhedral', idx, count*dim, false, ... 
-             { count, dim, false, coeffs } };
+             { count, dim, interleaved, coeffs } };
 
 end
