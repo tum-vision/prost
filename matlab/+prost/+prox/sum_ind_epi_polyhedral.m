e145e2777ab455b829544c8bb02a72c65a838269
Thomas Möllenhoff <moellenh@in.tum.de>
2016-04-01 17:36:24 +0200

    added nonconvex primal-dual example

--- matlab/+prost/+prox/sum_ind_epi_polyhedral.m
+++ matlab/+prost/+prox/sum_ind_epi_polyhedral.m
@@ -1,6 +1,4 @@
 function [prox] = sum_ind_epi_polyhedral(idx, count, dim, interleaved, a, b, count_vec, index_vec)
-% computes projection onto epigraph of function given as maximum
-% over linear functions specified by a, b, count_vec, index_vec.
     
     coeffs = { a, b, count_vec, index_vec };
     prox = { 'ind_epi_polyhedral', idx, count*dim, false, ... 
--- matlab/examples/example_nonconvex_rof.m
+++ matlab/examples/example_nonconvex_rof.m
@@ -7,7 +7,7 @@ f = double(im(:)) / 255.; % convert to [0, 1]
 %%
 % parameters
 grad = spmat_gradient2d(nx,ny,nc);
-lmb = 0.3;
+lmb = 3;
 
 %%
 % problem
@@ -15,8 +15,8 @@ u = prost.variable(nx*ny*nc);
 q = prost.variable(2*nx*ny*nc);
 
 u.fun = prost.function.sum_1d('square', 1, f, lmb, 0, 0);
-q.fun = prost.function.sum_norm2(... 
-    2 * nc, false, 'ind_leq0', 1, 1, 1, 0, 0);
+q.fun = prost.function.conjugate(prost.function.sum_norm2(... 
+    2 * nc, false, 'l0', 1, 0, 1, 0, 0));
 
 prost.set_dual_pair(u, q, prost.linop.sparse(grad));
 
@@ -26,21 +26,14 @@ prob = prost.min_max( {u}, {q} );
 % specify solver options
 backend = prost.backend.pdhg('stepsize', 'alg2', ...
                              'residual_iter', -1, ...
-                             'alg2_gamma', 0.05 * lmb);
+                             'alg2_gamma', lmb);
 
-pd_gap_callback = @(it, x, y) example_rof_pdgap(it, x, y, grad, ...
-                                                f, lmb, ny, nx, nc);
-
-opts = prost.options('max_iters', 20000, ...
-                     'interm_cb', pd_gap_callback, ...
-                     'num_cback_calls', 100, ...
-                     'verbose', false);
+opts = prost.options('max_iters', 500, ...
+                     'num_cback_calls', 10, ...
+                     'verbose', true);
 
 tic;
-prost.init();
-prost.set_gpu(0);
 result = prost.solve(prob, backend, opts);
-prost.release();
 toc;
 
 %%
