e5e1afccdea90012dac4581e1f536a6254f6c2c2
Thomas Möllenhoff <moellenh@in.tum.de>
2016-04-04 10:33:57 +0200

    asdf

--- matlab/examples/example_nonconvex_rof.m
+++ matlab/examples/example_nonconvex_rof.m
@@ -1,6 +1,6 @@
 %%
 % load input image
-im = imread('../../images/dog.png');
+im = imread('../../images/cow.png');
 [ny, nx, nc] = size(im);
 f = double(im(:)) / 255.; % convert to [0, 1]
 
@@ -26,9 +26,9 @@ prob = prost.min_max( {u}, {q} );
 % specify solver options
 backend = prost.backend.pdhg('stepsize', 'alg2', ...
                              'residual_iter', -1, ...
-                             'alg2_gamma', lmb);
+                             'alg2_gamma', 0.1 * lmb);
 
-opts = prost.options('max_iters', 500, ...
+opts = prost.options('max_iters', 2000, ...
                      'num_cback_calls', 10, ...
                      'verbose', true);
 
@@ -38,4 +38,4 @@ toc;
 
 %%
 % show result
-imshow(reshape(u.val, [ny nx nc]));
+imshow([double(im)/255 reshape([u.val], [ny nx nc])]);
