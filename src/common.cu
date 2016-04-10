e1ef1570958490bbe710182f73c215cb6d4b0317
Thomas Möllenhoff <moellenh@in.tum.de>
2016-04-10 18:20:33 +0200

    Minor fixes.

--- src/common.cu
+++ src/common.cu
@@ -61,26 +61,26 @@ void csr2csc(
   col_start[0] = 0;
 }
 
-// Beginning of GPU Architecture definitions                                                                                                                                                                                                  
+// Beginning of GPU Architecture defintions
 int _ConvertSMVer2Cores(int major, int minor)
 {
-  // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM                                                                                                                                            
+  // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM                                                            
   typedef struct
   {
-    int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version                                                                                                                                                
+    int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
     int Cores;
   } sSMtoCores;
 
   sSMtoCores nGpuArchCoresPerSM[] =
     {
-      { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class                                                                                                                                                                                
-      { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class                                                                                                                                                                                
-      { 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class                                                                                                                                                                               
-      { 0x32, 192}, // Kepler Generation (SM 3.2) GK10x class                                                                                                                                                                               
-      { 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class                                                                                                                                                                               
-      { 0x37, 192}, // Kepler Generation (SM 3.7) GK21x class                                                                                                                                                                               
-      { 0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class                                                                                                                                                                              
-      { 0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class                                                                                                                                                                              
+      { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+      { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+      { 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class
+      { 0x32, 192}, // Kepler Generation (SM 3.2) GK10x class
+      { 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class
+      { 0x37, 192}, // Kepler Generation (SM 3.7) GK21x class
+      { 0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class
+      { 0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class
       {   -1, -1 }
     };
 
